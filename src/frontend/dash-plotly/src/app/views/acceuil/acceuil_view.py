95f12950c57451d8b4032092c3c5cebf6168813c
Rushclin02 <takamrushclin@gmail.com>
2023-06-06 07:10:59 +0100

    Change

--- src/frontend/dash-plotly/src/app/views/acceuil/acceuil_view.py
+++ src/frontend/dash-plotly/src/app/views/acceuil/acceuil_view.py
@@ -2,16 +2,28 @@ from dash import html
 
 from components.header import HeaderComponent
 
+from components.title_page import TitlePageComponent
+
+
 class AcceuilView:
     def __init__(self) -> None:
         self.header = HeaderComponent()
+        self.title_page = TitlePageComponent()
 
     def render(self):
-        return html.Div([
-            self.header.render(),
-            html.Section([
-                html.Div([
-                    html.H1("Bonjour le genie Rushclin")
-                ], className="container-fluid")
-        ], className="section")]
+        return html.Div(
+            [
+                self.header.render(),
+                html.Section(
+                    [
+                        html.Div(
+                            [
+                                self.title_page.render("Acceuil"),
+                            ],
+                            className="container-fluid",
+                        )
+                    ],
+                    className="section",
+                ),
+            ]
         )
