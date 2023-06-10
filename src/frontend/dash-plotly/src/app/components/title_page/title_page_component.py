47b0da6593b7c7c3323f815c34e84d7cf2f7eed3
Rushclin02 <takamrushclin@gmail.com>
2023-06-10 10:47:38 +0100

    Page aceuil

--- src/frontend/dash-plotly/src/app/components/title_page/title_page_component.py
+++ src/frontend/dash-plotly/src/app/components/title_page/title_page_component.py
@@ -1,11 +1,11 @@
-from dash import html
+from dash import html, dcc
 
 
 class TitlePageComponent:
     def __init__(self) -> None:
         pass
 
-    def render(self, title: str):
+    def render(self, title: str, description=""):
         return html.Div(
             html.Div(
                 [
@@ -15,7 +15,22 @@ class TitlePageComponent:
                             className="title mb-30",
                         ),
                         className="col-md-6",
-                    )
+                    ),
+                    html.Div(
+                        html.Div(
+                            html.Nav(
+                                html.Ol(
+                                    html.Li(
+                                        dcc.Link(description, href=""),
+                                        className="breadcrumb-item",
+                                    ),
+                                    className="breadcrumb",
+                                )
+                            ),
+                            className="breadcrumb-wrapper text-end",
+                        ),
+                        className="col-md-6",
+                    ),
                 ],
                 className="row align-items-center",
             ),
