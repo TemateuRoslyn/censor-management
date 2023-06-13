838ef6a6d69ccc662b346e38f42208599a82dbad
Rushclin02 <takamrushclin@gmail.com>
2023-06-13 14:05:32 +0100

    Evoltion

--- src/frontend/dash-plotly/src/app/callbacks/layout_callbacks/toogle_sidebar_callback.py
+++ src/frontend/dash-plotly/src/app/callbacks/layout_callbacks/toogle_sidebar_callback.py
@@ -30,3 +30,8 @@ class ToogleSidebarCallback:
                     main_class.replace(" active", ""),
                     "close",
                 ]
+    def register_sidebar_callbacks(self):
+        @self.app.callback(
+            Output('active-nav-item', 'className'),
+            Input('', '')
+        )
