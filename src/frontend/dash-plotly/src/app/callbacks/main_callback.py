7662e621ecd86fa0f6f82b481572c9cae3e9b5d3
Rushclin <68521991+Rushclin@users.noreply.github.com>
2023-06-28 18:17:33 +0200

    Page acceuil (#17)
    * Mise en place du docker--mais ne marche pas encore et de l'architecture
    
    * Mise en place du layout principal
    
    * Add Settings of VsCode
    
    * Add CSS and JS files
    
    * Add Images
    
    * Add Callbaks
    
    * Add Button
    
    * Add Divider
    
    * Add Header component
    
    * Add components
    
    * Add Layout
    
    * Add about
    
    * Add Acceuil
    
    * Add Statistiques view
    
    * Change app.py
    
    * Add Title page Component
    
    * Change
    
    * Delete files
    
    * Change main.css
    
    * Evolution
    
    * Change Callbacks
    
    * Change Header
    
    * Edit Acceuil View
    
    * Add Graphe View
    
    * Add Notifications View
    
    * Add Parametrage view
    
    * Add Transformations View
    
    * Delete Welome View
    
    * Work in progress
    
    * Services
    
    * Page aceuil
    
    * Evolution
    
    * Ajout de l'option de Toggle sur la navbar
    
    * Restructuration des callbacks
    
    * Evoltion
    
    * Debut ajout de la fenetre modal pour le parametrage des graphes de l'acceuil
    
    * Travail sur la page de statistique
    
    * Mise en place du docker--mais ne marche pas encore et de l'architecture
    
    * parent 919be80f966e469a67ac8c1604a948a1dae2925d
    author Rushclin02 <takamrushclin@gmail.com> 1685709950 +0100
    committer Rushclin02 <takamrushclin@gmail.com> 1687613575 +0100
    
    parent 919be80f966e469a67ac8c1604a948a1dae2925d
    author Rushclin02 <takamrushclin@gmail.com> 1685709950 +0100
    committer Rushclin02 <takamrushclin@gmail.com> 1687613555 +0100
    
    parent 919be80f966e469a67ac8c1604a948a1dae2925d
    author Rushclin02 <takamrushclin@gmail.com> 1685709950 +0100
    committer Rushclin02 <takamrushclin@gmail.com> 1687442128 +0100
    
    Mise en place du docker--mais ne marche pas encore et de l'architecture
    
    * Edit Init file for callbacks
    
    * Close #10
    
    * Change
    
    * Evolution sur la page d'aceuil avec les differents graphes

--- src/frontend/dash-plotly/src/app/callbacks/main_callback.py
+++ src/frontend/dash-plotly/src/app/callbacks/main_callback.py
@@ -1,16 +1,14 @@
 from callbacks.layout_callbacks.render_callback import RenderCallback
 from callbacks.layout_callbacks.toogle_sidebar_callback import ToogleSidebarCallback
-from callbacks.accounts.register_control import RegisterControlCallback
-from callbacks.accounts.login_callback import LoginCallback
-from callbacks.graphs.switching_graphs import Switch
-from callbacks.interval.interval import Interval
+from callbacks.acceuil.acceuil_callback import AcceuilCallback
+from callbacks.acceuil.acceuil_state_callback import AcceuilStateCallback
+from callbacks.acceuil.acceuil_camemberg_callback import AcceuilCamembergCallback
+
 
 class MainCallback:
     def __init__(self, app) -> None:
         RenderCallback(app).register()
         ToogleSidebarCallback(app).register()
-        RegisterControlCallback(app).render_callbacks()
-        LoginCallback(app).render_callbacks()
-        Switch(app).switchCallback()
-        Interval(app).intervalCallback()
-        # ToogleSidebarCallback(app).register_sidebar_callbacks()
+        AcceuilCallback(app).register()
+        AcceuilStateCallback(app).register()
+        AcceuilCamembergCallback(app).register()
--- src/frontend/dash-plotly/src/app/views/acceuil/acceuil_view.py
+++ src/frontend/dash-plotly/src/app/views/acceuil/acceuil_view.py
@@ -1,11 +1,14 @@
-from dash import html
+from dash import html, dcc
+import plotly.graph_objects as go
+import numpy as np
 
 from components.header import HeaderComponent
 from components.title_page import TitlePageComponent
-from components.toasts.simple_toast import SimpleToast
 
 from views.acceuil.components.capteur_component import CapteurComponent
 from views.acceuil.components.sparkline_component import SparkLineComponent
+from views.acceuil.components.health_card import HealthCard
+from views.acceuil.components.camemberg_component import CamembergComponent
 
 
 class AcceuilView:
@@ -14,26 +17,134 @@ class AcceuilView:
         self.title_page = TitlePageComponent()
         self.capteur = CapteurComponent()
         self.sparkline = SparkLineComponent()
-        self.simple_toast = SimpleToast()
+        self.health = HealthCard()
+        self.camemberg_1 = CamembergComponent()
+        self.x0 = np.random.randn(500)
+        self.x1 = np.random.randn(500) + 1
+        self.histogramme = go.Figure(
+            layout={
+                "margin": {
+                    "l": 50,
+                    "r": 50,
+                    "t": 50,
+                    "b": 50,
+                },
+                "hovermode": False,
+                "showlegend": False,
+            },
+        )
+        self.histogramme.add_trace(
+            go.Histogram(
+                x=self.x0,
+                histnorm="percent",
+                name="Capteur 1",
+                xbins=dict(start=0, end=11.0, size=1.0),
+                marker_color="#EB89B5",
+                opacity=0.75,
+            )
+        )
+        self.histogramme.add_trace(
+            go.Histogram(
+                x=self.x1,
+                histnorm="percent",
+                name="Capteur 2",
+                xbins=dict(start=0, end=11.0, size=1.0),
+                marker_color="#330C73",
+                opacity=0.75,
+            )
+        )
+        self.histogramme.update_layout(
+            xaxis_title_text="Période",
+            yaxis_title_text="Valeur",
+            bargap=0.2,
+            bargroupgap=0.1,
+        )
 
     def render(self):
         return html.Div(
             [
-                self.simple_toast.render(
-                    msg="Plonger dans un bain de donnees !",
-                    title="BIENVENUE",
-                    cstyle="bg-primary position-absolute ml-70 mt-70 p-50",
-                    ico="primary"
-                ),
                 self.header.render(),
                 html.Section(
                     [
                         html.Div(
                             [
                                 self.title_page.render(
-                                    "ACCUEIL",
-                                    description="Un appercu global de tous les capteurs disponibles !",
+                                    "Dashboard",
+                                    description="",
                                 ),
+                                html.Div(
+                                    [
+                                        html.Div(
+                                            self.health.render(
+                                                icon="trending_up",
+                                                etat="Etat inconnue",
+                                                title="Santé des capteurs",
+                                                id="id_capt_health",
+                                            ),
+                                            className="col-md-3 col-xl-3 col-lg-4 col-sm-6",
+                                        ),
+                                        html.Div(
+                                            self.health.render(
+                                                icon="trending_up",
+                                                etat="Etat inconnue",
+                                                title="Accélerometres",
+                                                id="id_acc_health",
+                                            ),
+                                            className="col-md-3 col-xl-3 col-lg-4 col-sm-6",
+                                        ),
+                                        html.Div(
+                                            self.health.render(
+                                                icon="database",
+                                                etat="Inconnue",
+                                                title="Capteurs thermiques",
+                                                id="id_ther_health",
+                                            ),
+                                            className="col-md-3 col-xl-3 col-lg-4 col-sm-6",
+                                        ),
+                                        html.Div(
+                                            self.health.render(
+                                                icon="account_tree",
+                                                etat="Inconnue",
+                                                title="Capteurs de pression",
+                                                id="id_pres_health",
+                                            ),
+                                            className="col-md-3 col-xl-3 col-lg-4 col-sm-6",
+                                        ),
+                                    ],
+                                    className="row mt-3",
+                                ),
+                                html.Div(
+                                    [
+                                        self.camemberg_1.render(
+                                            title="Type de Capteurs",
+                                            id="types_capteurs",
+                                        ),
+                                        self.camemberg_1.render(
+                                            title="Capteurs les plus actifs",
+                                            id="capteurs_actifs",
+                                        ),
+                                        html.Div(
+                                            html.Div(
+                                                [
+                                                    html.Div(
+                                                        html.H6(
+                                                            "Santé des capteurs sous forme de graphe"
+                                                        )
+                                                    ),
+                                                    html.Div(
+                                                        dcc.Graph(
+                                                            figure=self.histogramme,
+                                                        )
+                                                    ),
+                                                ],
+                                                className="card-style mb-3",
+                                            ),
+                                            className="col-lg-12 col-md-12 col-sm-12 col-sx-12",
+                                        ),
+                                    ],
+                                    className="row mt-3",
+                                ),
+                                html.Div(className="row mt-3"),
                                 html.Div(
                                     [
                                         html.Div(
@@ -88,13 +199,4 @@ class AcceuilView:
                     className="section",
                 ),
             ]
-        )
-    def render(self):
-        return html.Div([
-            self.header.render(),
-            html.Section([
-                html.Div([
-                    html.H1("Bonjour le genie Rushclin")
-                ], className="container-fluid")
-        ], className="section")]
         )
