01b35b452f85f82ab0c289ff85707c8ed209b1dd
Merritz <morritzngueguim43@gmail.com>
2023-07-01 07:13:18 -0400

    updating map

--- src/frontend/dash-plotly/src/app/views/track/tracking_view.py
+++ src/frontend/dash-plotly/src/app/views/track/tracking_view.py
@@ -14,28 +14,47 @@ class TrackingView:
 
     def render(self):
         insert = requests.get("http://127.0.0.1:8000/tracking/insert?city=Paris&state=France&lat=48.866667&lon=2.333333")
-        datas = requests.get("http://127.0.0.1:8000/tracking/next")
+        datas = requests.get("http://127.0.0.1:8000/tracking/next").json()
 
 
-        print(datas)
-        fig = px.scatter_mapbox(
-            data_frame=datas.json(),
-            lat="lat",
-            lon="lon",
-            hover_name="city",
-            hover_data=["state"],
-            zoom=15,
-            height=350,
-            title="Tracker"
+        print(datas.get('lat'))
+        fig = go.Figure(
+            data=go.Scattermapbox(
+            mode="markers+lines",
+            lat=datas.get('lat'),
+            lon=datas.get('lon'),
+            marker={'size':20},
+            )
         )
-        fig.update_layout(mapbox_style="open-street-map")
-        fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
-        # fig.update_layout(mapbox_bounds={"west": 0, "east": 10, "south": 0, "north": 51})
+
 
         fig.update_layout(
-            autosize=True,
-            hovermode='closest'
+            margin={"r": 0, "t": 0, "l": 0, "b": 0},
+            mapbox={
+                'style':'open-street-map',
+                'zoom':5,
+                'center':{'lon':2.320041, 'lat':48.8588897}
+            }
         )
+        # fig.show()
+        # fig = px.scatter_mapbox(
+        #     data_frame=datas,
+        #     lat="lat",
+        #     lon="lon",
+        #     hover_name="city",
+        #     hover_data=["state"],
+        #     zoom=15,
+        #     height=350,
+        #     title="Tracker"
+        # )
+        # fig.update_layout(mapbox_style="open-street-map")
+        # fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
+        # # fig.update_layout(mapbox_bounds={"west": 0, "east": 10, "south": 0, "north": 51})
+
+        # fig.update_layout(
+        #     autosize=True,
+        #     hovermode='closest'
+        # )
         return html.Div(
             [
                 self.header.render(),
@@ -48,8 +67,8 @@ class TrackingView:
                                 id="track-interval",
                                 disabled=False,
                                 n_intervals=0,
-                                interval=3000,
-                                max_intervals=-1
+                                interval=10000,
+                                max_intervals=0
                             ),
                             html.Div(
                                 [
