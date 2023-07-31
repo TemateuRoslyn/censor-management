69e7cd4ba5c769a56128dc46a957779826801c5a
Temateu Roslyn <temateuroslynf32@gmail.com>
2023-07-31 20:20:06 +0100

    saving

--- deployment/charts/mongodb/run.sh
+++ deployment/charts/mongodb/run.sh
@@ -1,6 +1,6 @@
-kubectl apply -f storageclass.yaml -f nodeport.yaml
+kubectl apply -f nodeport.yaml
 
-helm install censor-mongodb-deployment bitnami/mongodb -n censor
+helm install censor-mongodb-deployment -f values.yaml bitnami/mongodb -n censor
 
 # mise a jour d'une chart (pas besoin de l ereinstaller)
 helm upgrade censor-mongodb-deployment -f values.yaml bitnami/redis -n censor
