78637b0aba19d1f527e83ed5e8002ec6e5f82164
deffosic <deffocedric91@gmail.com>
2023-08-28 22:02:24 +0200

    Ajout d'un agregateur (#50)

---
+++ src/backend/python/Sensor_management-prod/aggregator/src/redis_stream.py
@@ -0,0 +1,94 @@
+import redis
+
+class RedisStreamReader:
+    def __init__(self, redis_host='localhost', redis_port=6379, stream_name='mystream', consumer_group='mygroup', consumer_name='myconsumer'):
+        self.redis_host = redis_host
+        self.redis_port = redis_port
+        self.streams = stream_name
+        self.consumer_group = consumer_group
+        self.consumer_name = consumer_name
+        self.redis_conn = redis.Redis(host=self.redis_host, port=self.redis_port, decode_responses=True)
+    
+    def create_consumer_group(self):
+        try:
+            self.redis_conn.xgroup_create(self.streams, self.consumer_group, id='0', mkstream=True)
+            print(f"Consumer group '{self.consumer_group}' created.")
+        except redis.exceptions.ResponseError as e:
+            if "BUSYGROUP Consumer Group name already exists" in str(e):
+                print(f"Consumer group '{self.consumer_group}' already exists.")
+            else:
+                print(f"Error creating consumer group: {e}")
+    
+    def read_stream(self):
+        try:
+
+            if type(self.streams)=="str":
+                while True:
+                    messages = self.redis_conn.xreadgroup(groupname=self.consumer_group, consumername=self.consumer_name,
+                                                        streams={self.stream_name: '>'}, count=1, block=0)
+                    for _, message in messages:
+                        message_id = message[0]
+                        message_data = message[1]
+                        print(f"Message ID: {message_id}, Message Data: {message_data}")
+            else:
+                while True:
+                # Prepare the streams for xreadgroup in the format ('stream_name', '>')
+                    streams_with_start = [(stream, '>') for stream in self.streams]
+                    
+                    messages = self.redis_conn.xreadgroup(groupname=self.consumer_group, consumername=self.consumer_name,
+                                                        streams=streams_with_start, count=1, block=0)
+                    for stream_name, message_list in messages:
+                        message_id = message_list[0][0]
+                        message_data = message_list[0][1]
+                        print(f"Stream: {stream_name}, Message ID: {message_id}, Message Data: {message_data}")
+        
+        except KeyboardInterrupt:
+            print("Stream reading stopped by user.")
+
+    def last_delivery_id(self, stream):
+
+        lastid='>'
+
+        lis = self.redis_conn.xinfo_groups(stream)
+
+        if len(lis)!=0:
+            for elt in lis:
+                if elt['name']==self.consumer_group:
+                    lastid = elt['last-delivered-id']
+
+        return lastid
+
+    
+    def read_one_streams(self):
+        try:
+            # Prepare the streams and last-delivered IDs for xreadgroup
+            streams_with_start = [(stream, self.last_delivery_id(stream)) for stream in self.streams]
+            # last_delivered_ids = {stream: '>' for stream in self.streams}
+                
+            messages = self.redis_conn.xreadgroup(groupname=self.consumer_group, consumername=self.consumer_name,
+                                                      streams=streams_with_start, count=1, block=0)
+            if messages is not None:
+
+                for stream_name, message_list in messages:
+
+                    if len(message_list[0])>0:
+                        message_id = message_list[0][0]
+                        # message_data = message_list[0][1]
+                        # last_delivered_ids[stream_name] = message_id
+                        # print(f"Stream: {stream_name}, Message ID: {message_id}, Message Data: {message_data}")
+                        # Acknowledge the message
+                        self.redis_conn.xack(stream_name, self.consumer_group, message_id)
+                        print(f"Message ID {message_id} acknowledged.")
+
+            return messages
+        except KeyboardInterrupt:
+            print("Stream reading stopped by user.")
+    
+    def write(self, stream, data):
+
+        self.redis_conn.xadd(stream, data)
+
+    def close(self):
+        self.redis_conn.close()
+    
+
--- src/backend/python/Sensor_management-prod/udp_packets/src/test.py
+++ src/backend/python/Sensor_management-prod/udp_packets/src/test.py
@@ -1,13 +1,62 @@
 from udp_protocol import UDPHandler
 
+import pandas as pd
+
 
 if __name__=='__main__':
 
-	udp = UDPHandler(target_ip='0.0.0.0', target_port=55000)
+	# udp = UDPHandler(target_ip='0.0.0.0', target_port=55000)
 
-	while True:
-		print(f"packet en attente")
+	# while True:
+	# 	print(f"packet en attente")
 
-		data = udp.receive_packet()
+	# 	data = udp.receive_packet()
    
-		print(f"{data}")
+	# 	print(f"{data}")
+
+	a = {
+		'timestamp':[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21],
+		'ai0': [elt for elt in range(20,41)],
+		'ai1': [elt for elt in range(60,81)],
+		'ai2': [elt for elt in range(120,141)]
+	}
+
+	b = {
+		'timestamp':[1,2,3,4,5,6,7,8,9,15,16,17,18,24,25],
+		'udp': [elt for elt in range(20,35)]
+	}
+
+	c = {
+		'timestamp':[25,26,32,41,50,66,72,81,92,98],
+		'tor1': [elt for elt in range(20,30)],
+		'tor2': [elt for elt in range(20,30)]
+	}
+
+	# print(f"{a}\n")
+	# print(f"{b}\n")
+	# print(f"{c}")
+
+	a_pd = pd.DataFrame(a)
+
+	b_pd =pd.DataFrame(b)
+
+	c_pd =pd.DataFrame(c)
+
+	ab_pd = a_pd.merge(b_pd, on=['timestamp'], how='outer')
+
+	ac_pd = a_pd.merge(c_pd, on=['timestamp'], how='outer')
+
+	# print(f"{a_pd.head()}\n")
+
+	# print(f"{b_pd.head()}\n")
+
+	# print(f"{c_pd.head()}\n")
+
+	# print(f"{ab_pd}\n")
+
+	# print(f"{ab_pd.fillna(method='ffill')}\n")
+
+
+	print(f"{ac_pd}\n")
+
+	print(f"{ac_pd.fillna(method='ffill')}\n")
